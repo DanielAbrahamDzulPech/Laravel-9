453cd630f5faea59ce46bd69e7d6ac211b8dee4e
Daniel Abraham Dzul Pech <danieldayamond@gmail.com>
2022-06-26 19:52:40 -0500

    Cambios de home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="">
+        <h1 class="bienvenida">Bienvenido a 5W</h1>
+        <p class="parrafo">Con nuestra aplicación web usted podrá manejar todas las tareas administrativas de su taller a un costo reducido, unas maximas prestaciones y un servicio de soporte 24/4. Podrá visualizar su agenda y la de sus empleados,emitir presupuestos a sus clientes, llevar un control del stock, tener un control documental de sus dosumentos importantes.<br>Podrá adjuntar fotos a las ordenes, directamente desde el móvil o una tableta, sin pasar por un ordenador.</p>
+    </div>
+</body>
+</html>
+
+<style>
+    .bienvenida{
+        text-align: center;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+    }
+    .parrafo{
+        padding-top: 2rem;
+        padding-right: 10rem;
+        padding-left: 10rem;
+        padding-bottom: 2rem;   
+    }
+</style>
